a9deff06d1cfe9b2a0b8a9f0e338eaec205c40a0
Ashish11212776 <ashish.kumar@avisoft.io>
2025-01-22 17:12:06 +0530

    vertion1

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+// https://api.thenewsapi.com/v1/news/top?api_token=5le45unAd3gsS4lLT8VzoSmrXjHh0LnadDd1tEhF&locale=us&limit=3
