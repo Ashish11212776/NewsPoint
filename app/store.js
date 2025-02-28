05e853fb0578174c22db1d7711bfd2cd303d4fcb
Ashish Kumar <ashish.kumar@avisoft.io>
2025-02-28 18:49:47 +0530

    Update store.js

--- app/store.js
+++ app/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit'
+ import { configureStore } from '@reduxjs/toolkit'
 import crudReducers from '../src/feacture/crudSlice.js'
 export const store=configureStore({
    reducer:{
